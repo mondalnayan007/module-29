998df4f7f6d83a34f7cd94fc61d1b5a9592413ab
Nayan <n007kingkong@gmail.com>
2025-08-30 00:47:57 +0600

    arrow function

---
+++ arrow-function.js
@@ -0,0 +1,20 @@
+
+
+const multiply = (num1,num2) => num1*num2
+
+
+
+const ans = multiply(5,6);
+// console.log(ans)
+
+const doMultiple = (a ,b) => {
+    const sum = a+b;
+    const mul = a*b;
+    const sub = a-b;
+    const total = sum + mul + sub ;
+    return total;
+
+}
+
+const result = doMultiple(40 , 10);
+console.log(result);
